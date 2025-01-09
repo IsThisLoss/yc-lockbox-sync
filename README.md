3e192209d1bd5d4fca7fac219da00a42ec5f6c85
IsThisLoss <kopturovdim@gmail.com>
2025-01-09 22:56:53 +0300

    write to file

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # yc-lockbox-sync
+
 A tool to synchronize Yandex Cloud Lockbox secret to a file
+
+## Usage
+
+```bash
+./yc-lockbox-sync --credentials ./authorized_keys.json --secret-id e6qclp5agq8e7oe2hpgg --secret-key .env --dst ./.env
+```
+
+Parameters:
+
+`credentials` - path to file with credentials of service account
+`secret-id` - id of the secret to sync in Yandex Cloud Lockbox
+`secret-key` - key inside the secret to sync
+`dst` - output file path
--- main.go
+++ main.go
@@ -3,42 +3,64 @@ package main
 import (
 	"context"
 	"flag"
-	"log"
+	"fmt"
+	"os"
 
 	"github.com/yandex-cloud/go-genproto/yandex/cloud/lockbox/v1"
 	"github.com/yandex-cloud/go-sdk/iamkey"
 	ycsdk "github.com/yandex-cloud/go-sdk"
 )
 
+func panicOnError(err error) {
+  if err != nil {
+    panic(err)
+  }
+}
+
 func main() {
 	authKeys := flag.String("credentials", "", "Path to authorized keys of service account")
 	secretID := flag.String("secret-id", "", "Your Yandex.Cloud Lockbox ID of the secret")
+	secretKey := flag.String("secret-key", "", "Key inside the secret to save")
+	dst := flag.String("dst", "", "Destination file")
   flag.Parse()
 
   key, err := iamkey.ReadFromJSONFile(*authKeys)
-  if err != nil {
-    panic(err)
-  }
+  panicOnError(err)
 
   cred, err := ycsdk.ServiceAccountKey(key)
-  if err != nil {
-    panic(err)
-  }
+  panicOnError(err)
 
 	sdk, err := ycsdk.Build(context.Background(), ycsdk.Config{
 		Credentials:  cred,
 	})
-	if err != nil {
-		panic(err)
-	}
+  panicOnError(err)
 
-	p, err := sdk.LockboxPayload().Payload().Get(context.Background(), &lockbox.GetPayloadRequest{
+	payload, err := sdk.LockboxPayload().Payload().Get(context.Background(), &lockbox.GetPayloadRequest{
 		SecretId: *secretID,
 	})
-  if err != nil {
-    panic(err)
+  panicOnError(err)
+
+  data := []byte{}
+  for _, entry := range payload.Entries {
+    if entry.Key == *secretKey {
+      binVal := entry.GetBinaryValue() 
+      if binVal != nil {
+        data = binVal
+      } else {
+        data = []byte(entry.GetTextValue())
+      }
+      break
+    }
   }
 
+  if len(data) == 0 {
+    panic("Key was not found")
+  }
 
-	log.Println(p)
+  out, err := os.Create(*dst)
+  panicOnError(err)
+
+  out.Write(data)
+
+	fmt.Println("Done")
 }
